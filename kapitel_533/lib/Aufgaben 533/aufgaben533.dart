23780ef762c79d0c335bc1d5c650b351af09dc76
Vanya <vanya-grabbe@t-online.de>
2023-09-04 15:35:38 +0200

    mit Aufgaben 2 und 3 von Kapitel 541 ergänzt

--- kapitel_533/lib/Aufgaben 533/aufgaben533.dart
+++ kapitel_533/lib/Aufgaben 533/aufgaben533.dart
@@ -11,66 +11,74 @@ class Aufgaben533 extends StatefulWidget {
   @override
   State<Aufgaben533> createState() => _Aufgaben533State();
 }
+
 class _Aufgaben533State extends State<Aufgaben533> {
-  
   List<File> images = [];
   pickImage(ImageSource src) async {
-    final pickedImage = await ImagePicker().pickImage(source: src);
+    try {
+     int result;
+for (int i = 10; i >= 0; i--) {
+result = 10 ~/ i;
+print(result);
+}
+      final pickedImage = await ImagePicker().pickImage(source: src);
     setState(() {
       imagePath = pickedImage?.path ?? '';
       images.add(File(imagePath));
     });
+    }
+    catch (error){
+    print("Hallo"+error.toString());
+    }
+    
   }
-  void _removeImage(int index){
+  
+
+  void _removeImage(int index) {
     setState(() {
       images.removeAt(index);
     });
   }
-String imagePath = '';
+
+  String imagePath = '';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-        title: const Text('Aufgabe1'),
-      ),
-      body: 
-          Column(
-            children: [
-               MaterialButton(
-                  onPressed: () {
-                    pickImage(ImageSource.camera);
-                  },
-                  child: const Text('Kamera')),
-              const SizedBox(width: 32),
-              MaterialButton(
-                  onPressed: () {
-                    pickImage(ImageSource.gallery);
-                  },
-                  child: const Text('Galerie')),
-               Expanded(
-                 child: ListView.builder(itemCount: images.length, itemBuilder:(context, index) {
-                   return GestureDetector(
+          title: const Text('Aufgabe1'),
+        ),
+        body: Column(
+          children: [
+            MaterialButton(
+                onPressed: () {
+                  pickImage(ImageSource.camera);
+                },
+                child: const Text('Kamera')),
+            const SizedBox(width: 32),
+            MaterialButton(
+                onPressed: () {
+                  pickImage(ImageSource.gallery);
+                },
+                child: const Text('Galerie')),
+            Expanded(
+              child: ListView.builder(
+                itemCount: images.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
                     onTap: () => _removeImage(index),
                     child: Container(
                       padding: EdgeInsets.all(10),
                       child: Image.file(images[index]),
                     ),
-                   );
-                   
-                   //ListTile(title: Image.file(images[index]));
-                 },),
-               )
-             ],
-          ),
-        
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
-      );
-      
-    
+    );
   }
 }
-
-
-
